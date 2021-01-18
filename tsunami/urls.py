a3368e7ecfab2821437a66d1955d905ac0db4b4c
Jun Young Jang <junyoungjang@Junui-MacBookPro.local>
2021-01-18 14:26:03 +0900

    Fix: urls.py 수정

--- tsunami/urls.py
+++ tsunami/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
     path('products', include('products.urls')),
-=======
     path('users', include('users.urls'))
->>>>>>> main
 ]
